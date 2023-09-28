ea879c16b7b157f4da73eea390eb5419d5c92cde
GitHSharp <112707967+githsharp@users.noreply.github.com>
2023-09-28 19:49:43 +0200

    Commenting on server.js functionality

--- 4-shopping-list-starter/server.js
+++ 4-shopping-list-starter/server.js
@@ -1,37 +1,51 @@
-const fs = require('fs').promises;
-const path = require('path');
-const express = require('express');
-const bodyParser = require('body-parser');
+const fs = require("fs").promises;
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const fileName = 'groceries.txt';
+const fileName = "groceries.txt";
 
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-  const filePath = path.join(__dirname, 'pages', 'index.html');
+// "app.get"= start endpoint - som hämtar index-filen från pages-mappen (så man gör i node)
+// detta är en serverbaserad webapp som hämtar en fil från en mapp inte en api
+// använder sendFile för att skicka data till klienten
+app.get("/", (req, res) => {
+  const filePath = path.join(__dirname, "pages", "index.html");
   res.sendFile(filePath);
 });
 
-app.get('/groceries', (req, res) => {
-  const filePath = path.join(__dirname, 'groceries', fileName);
+// app.get("/groceries",... hämtar filen groceries.txt från mappen groceries
+// använder sendFile för att skicka data till klienten
+
+app.get("/groceries", (req, res) => {
+  const filePath = path.join(__dirname, "groceries", fileName);
   res.sendFile(filePath);
 });
 
-app.post('/add', (req, res) => {
-  const supply = req.body.grocery + '\n';
+// app.post("/add",... lägger till en vara i filen groceries.txt
+// + "\n" lägger till en radbrytning efter varje vara
+app.post("/add", (req, res) => {
+  const supply = req.body.grocery + "\n";
 
-  const filePath = path.join(__dirname, 'groceries', fileName);
+  const filePath = path.join(__dirname, "groceries", fileName);
 
-  fs.appendFile(filePath, supply, 'utf8')
-    .then(() => res.redirect('/'))
+  // appendFile lägger till en vara i filen groceries.txt
+  // den tar in det iva req.body.grocery och lägger till det i filen
+  // utf8 är en textkodning som används för att representera Unicode
+  // som kan hantera alla tecken i alla språk
+  // redirectar tillbaka till startsidan
+  fs.appendFile(filePath, supply, "utf8")
+    .then(() => res.redirect("/"))
     .catch((err) => {
       console.error(err);
     });
 });
 
-
-//app.listen(80);
-app.listen(process.env.PORT);
+// lyssnar på port 80
+app.listen(80);
+// Raden nedan hör ihop med varianten i DockerfileLEKTION
+//app.listen(process.env.PORT);
